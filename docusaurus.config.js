1e0b496e4a35641d718d35bdeeb3a0c5f5a3dbea
Sayan Nandan <nandansayan@outlook.com>
2024-06-15 11:56:05 +0530

    meta: Bump versions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,7 +125,7 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '0.8.2',
+              label: '0.8.3',
               path: '/',
             }
           },
